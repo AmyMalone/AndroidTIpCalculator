c30af7346702d6a67894a89d055fe208b25199e4
Amy Malone <amalone2@terpmail.umd.edu>
2013-07-21 22:50:09 -0400

    first commit of android tip calculator

---
+++ TipCalculator/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.tipcalculator;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    EditText origBill;
+    EditText origTipPerc;
+
+    TextView tipTot;
+    TextView billTotal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        origBill = (EditText)findViewById(R.id.editBillAmount);
+        origTipPerc = (EditText)findViewById(R.id.editPercentAmount);
+
+        tipTot = (TextView)findViewById(R.id.tipTotal);
+        billTotal = (TextView)findViewById(R.id.billTotal);
+
+        Button butCalc = (Button)findViewById(R.id.buttonCalculate);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    //this method called when "calculate" button clicked, as XML file specifies.
+    public void calculate(View view){
+        Double enteredBill = Double.valueOf(origBill.getText().toString());
+        Double enteredPerc = Double.valueOf(origTipPerc.getText().toString());
+
+        Double tipCalc = enteredBill*enteredPerc;
+        Double totalCalc = tipCalc+enteredBill;
+
+        tipTot.setText(tipCalc.toString());
+        billTotal.setText(totalCalc.toString());
+
+    }
+
+
+
+    
+}
